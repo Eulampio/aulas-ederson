7254529e3f72a7801cc546c4b80006c164f5a5b0
Eulampio <alesantoseulampio@gmail.com>
2024-10-15 18:33:53 -0300

    aula 06
    aula 06d5

---
+++ code/aulas-ederson06/atividade06-06.py
@@ -0,0 +1,22 @@
+# elif salario>280 and salario<700:
+#     percentual=15
+# elif salario>=700 and salario<1500:
+#     percentual=10
+# elif salario>=1500:
+#     percentual=5
+
+
+
+
+
+
+# aumento = (percentual/100)*salario
+# total=salario+aumento
+#D = a desconto L= litros t = total
+
+opcao=str(input("informe se e gasolina (a) ou alcool (b)"))
+if opcao== "a":
+    l=float(input("informe a guantidade de litros"))
+elif l<=20:
+p=3
+valort=(p/100)*l
