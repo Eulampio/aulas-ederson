9e020ffb1d5f060eb34571a867ba1f19d46ed240
Eulampio <alesantoseulampio@gmail.com>
2024-10-24 18:22:29 -0300

    aula de for
    aula 08

---
+++ code/aula-ederson07/aula-ederson-8/atividade08-01.py
@@ -0,0 +1,5 @@
+# faça uma calculadora
+for a in range (0,10,1):
+    for b in range (0,10,1):
+        x = a*b
+        print(a,"x",b, "=",x)
---
+++ code/aula-ederson07/aula-ederson-8/aula08.py
@@ -0,0 +1,44 @@
+nomes=["pedro","joao","leticia"]
+for n in nomes:
+    print(n)
+    nomes=["pedro","joao","leticia"]
+for n in nomes:
+    print(n)
+
+nome="alexandre"
+for n in nome:
+    print(n)
+break no for 
+nomes=["pedro","joao","leticia"]
+for n in nomes:     
+    print(n)
+    if n=="joao":
+     break
+
+nomes=["pedro","joao","leticia"]
+for n in nomes:     
+    if n=="joao":
+     continue
+    print(n)
+   continue no for  
+x=10
+while x>0:
+    x=x-1
+    if x==5 or x==0:
+        continue
+    print(x)
+ 
+
+
+for s in range(6): pode designar inicio e fim
+for s in range(-10,10,2):
+
+    print(s)
+
+for i in range (5):
+    for j in range (6):
+        print(i,j)
+
+nomes=["pedro","joao","leticia"]
+for n in nomes:     
+    print(n)
